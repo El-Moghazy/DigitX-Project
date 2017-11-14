b20672317e41baf485bb04158d715dcd787a9109
MoghazyCoder <abdelrhman.almoghazy@student.guc.edu.eg>
2017-11-14 10:44:22 +0200

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -11,69 +11,60 @@ from keras.models import Sequential
 from keras.utils import np_utils
 
 K.set_image_dim_ordering('th')
-# fix random seed for reproducibility
-seed = 7
+
+seed = 0
 numpy.random.seed(seed)
-# load data
-# test = pd.read_csv('test.csv')
-# training = pd.read_csv('train.csv')
-# X_test2 = test
-# train = ["pixel" + str(x) for x in range(784)]
-# X_train = training[train]
-# y_train = training['label']
-# X_test2 = X_test2.as_matrix()
-# X_train = X_train.as_matrix()
-# y_train = y_train.as_matrix()
 
 num_classes = 10
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-# reshape to be [samples][pixels][width][height]
+
+# Reshaping the samples according to theano convention samples,channels,rows,columns as we are using image_dim_ordering('th') if you are using 
+# image_dim_ordering('tf') you will need to change the order to samples,rows,column,channels and here we have only one channel because we are 
+# using the image in grayscale not RGB.
 X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
 X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
-# normalize inputs from 0-255 to 0-1
+
+# normalize the inputs by dividing / 255 because the maximum value that can be stored in the pixel is 255 so it will give the range from 0 to 1
 X_train = X_train / 255
 X_test2 = X_test / 255
-# one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
 
+# making the output in the form of one vs all (aka one hot encoding) which means that we will have 10 calsses from 0 to 10 one class or each number from 0 to 9
+y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 
+# Our model will be a sequentiol model consisting of two convolutional layer and two fully connected layers : Conv -> MaxPool -> Dropout -> Conv -> MaxPool -> Dropout -> fullyConnected -> Fully connected
+model = Sequential()
+model.add(Conv2D(30, (5, 5), input_shape=(1, 28, 28), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
-# define the larger model
-def model():
-    # create model
-    model = Sequential()
-    model.add(Conv2D(20, (5, 5), input_shape=(1, 28, 28), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(50, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
-    model.add(Flatten())
-    model.add(Dense(200, activation='relu'))
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(num_classes, activation='softmax'))
-    # Compile model
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
+model.add(Conv2D(70, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
+model.add(Flatten())
+model.add(Dense(300, activation='relu'))
+model.add(Dense(300, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+#Early Stopping
+#keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='auto')
+
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 
-# build the model
-model = model()
 # Fit the model
-model.fit(X_train, y_train, epochs=10, batch_size=200)
-# Final evaluation of the model
-# scores = model.evaluate(X_test, y_test, verbose=0)
-# print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
+model.fit(X_train, y_train, epochs=15, batch_size=200)
+
+
+# Evaluating the model on unseen data to be able to do the error analysis and know if we have bias or variance problems
+scores = model.evaluate(X_test, y_test, verbose = 10)
+
+# Saving the model for future use
 model_json = model.to_json()
 with open("model.json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
 model.save_weights("model.h5")
-print("Saved model to disk")
-
-result = model.evaluate(X_test, y_test, verbose=10)
-# result = numpy.argmax(result, axis=1)
-# result = pd.Series(result, name="Label")
-# submission = pd.concat([pd.Series(range(1, 28001), name="ImageId"), result], axis=1)
-# submission.to_csv("C:/Users/ElMoghazy/Desktop/f.csv", index=False)
+print("Model Saved successfully")
